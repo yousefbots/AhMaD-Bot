29a6c2f6e3437ecb41bc179677d36c0022247dd9
yousefbots <45495875+yousefbots@users.noreply.github.com>
2018-11-30 20:56:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`-play | Last Music`,"http://twitch.tv/AhMaD")
+client.user.setGame(`-play | AhMaD`,"http://twitch.tv/AhMaD")
 client.user.setStatus("dnd")
 });
 
